958266fd7fbeb8d3f66f4d41744d82a69d9d24f7
Richard-Damico <55593832+Richard-Damico@users.noreply.github.com>
2019-09-25 08:48:58 -0700

    Add files via upload

---
+++ 9.18.19/mathrandom.java
@@ -0,0 +1,22 @@
+
+
+public class mathrandom
+{
+    public static void main(String[] args)
+    {
+        for (int i = 0; i < 50; i += 1)
+        {
+            double randNum = Math.random();
+            System.out.println(randNum);
+        
+            
+        
+        
+        }
+    }
+    
+    
+    
+    
+    
+}
---
+++ Classwork7_9.23.19/CW7.java
@@ -0,0 +1,34 @@
+public class CW7
+{
+    public static void main(String[] args)
+    {
+        System.out.println(findSum(2,3));
+        
+    }
+    
+    public static double findSum(int a, int b)
+    {
+        return a + b;
+        
+    }
+
+
+    public static String canIVote(int age)
+    {
+        
+        if (age >= 18)
+            return "You Can Vote";
+          else
+            return "You Can't Vote";
+        }
+
+
+
+
+
+
+
+
+
+
+}   
