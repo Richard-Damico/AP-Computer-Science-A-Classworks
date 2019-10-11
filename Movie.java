b3d90665d685a200506856131358e9b142e3f56d
Richard-Damico <55593832+Richard-Damico@users.noreply.github.com>
2019-10-11 09:29:44 -0700

    Add files via upload

---
+++ Movie.java
@@ -0,0 +1,152 @@
+public class Movie
+{
+    
+ //instance variables are adjectives
+ private String title;
+ private String genre;
+ private double lengthInMin;
+ 
+ 
+ //constructor definitin
+ public Movie(String titl, String gen, double len)
+ {
+ 
+ title = titl;
+ genre = gen;
+ lengthInMin = len;
+ 
+ 
+ 
+ 
+ 
+}
+
+public void setTite(String t)
+{
+title = t;
+
+
+
+}
+
+public String getTitle()
+{
+ return title;   
+}
+
+
+public void setGenre(String g)
+{
+ genre = g;   
+    
+}
+
+
+public String getGenre()
+{
+ return genre;   
+}
+
+public void setLength(double l)
+{
+ lengthInMin = l;   
+    
+}
+
+
+public double getLength()
+{
+ return lengthInMin;   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}   
